cca06382d708e484f45efd9e7895107527119aff
Gabik <gabrielsmyk10@gmail.com>
2025-12-09 18:21:53 +0100

    ..

--- Program.cs
+++ Program.cs
@@ -14,8 +14,6 @@ namespace ZTP___Statki
         [STAThread]
         static void Main()
         {
-
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
